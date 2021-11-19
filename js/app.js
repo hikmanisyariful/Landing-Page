d1f178c00d885b35b7c6e9568714dd31380784d8
hikmanisyariful <syarifulfajar017@gmail.com>
2021-11-19 09:05:29 +0700

    Create new section
    Define global variable for "sections"

--- js/app.js
+++ js/app.js
@@ -22,6 +22,7 @@
  * Define Global Variables
  *
  */
+let sections;
 
 /**
  * End Global Variables
@@ -34,6 +35,45 @@
  * Begin Main Functions
  *
  */
+
+// Create new section
+const addNewSection = () => {
+  let number = document.getElementsByTagName("section").length + 1;
+  const main = document.getElementById("main");
+  main.innerHTML += `
+    <section id="section${number}" data-nav="Section ${number}" class="your-active-class">
+      <div class="landing__container">
+        <h2>Section ${number}</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+          fermentum metus faucibus lectus pharetra dapibus. Suspendisse
+          potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget
+          lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed
+          convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla
+          eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam
+          nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis
+          lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a
+          tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus
+          vitae elit. Integer nec libero venenatis libero ultricies molestie
+          semper in tellus. Sed congue et odio sed euismod.
+        </p>
+        <p>
+          Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar
+          gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam.
+          Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum
+          consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget
+          elementum tortor mollis non.
+        </p>
+      </div>
+    </section>
+  `;
+
+  sections = document.querySelectorAll("section");
+};
+
+addNewSection();
+addNewSection();
+// console.log(sections.length);
 
 // build the nav
 
